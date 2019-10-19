c8241c6bda872a77f031f710937d58d4f5effc26
Ayush Kumar <kmar.ayush@gmail.com>
2019-10-19 20:05:14 +1100

    added tslint

--- src/RoomGenerator.ts
+++ src/RoomGenerator.ts
@@ -1,21 +1,20 @@
 import { Room } from "room";
 
-module.exports = {
-    Generate: function () {
-        var text = "";  
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";      
-        for(var i = 0; i < 10; i++) {     
-            text += possible.charAt(Math.floor(Math.random() * possible.length));    
+export function Generate(): string {
+        let text = "";
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for (let i = 0; i < 10; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
         }
         return text;
-    },
+    }
 
-    Room : ():Room => {
-        let room : Room = {
-            code:'',
-            notes:[],
-            participants:[]
+export function EmptyRoom(): Room {
+        const room: Room = {
+            code: "",
+            notes: [],
+            participants: [],
         };
         return room;
     }
-};
+
--- src/index.ts
+++ src/index.ts
@@ -1,87 +1,81 @@
-import { Room, Participant, Note } from "room";
 import { Request, Response } from "express";
-import { Socket } from "socket.io";
-let express = require('express');
-let path = require('path');
-let app = express();
-let http = require('http').createServer(app);
-let io = require('socket.io')(http);
-let RoomGenerator = require('./RoomGenerator')
-let currentRooms : Room[] = [];
+import express from "express";
+import httpModule from "http";
+import path from "path";
+import { Note, Participant, Room } from "room";
+import socketModule, { Socket } from "socket.io";
+const app = express();
+const http = httpModule.createServer(app);
+const io = socketModule(http);
+import { EmptyRoom, Generate } from "./RoomGenerator";
+const currentRooms: Room[] = [];
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// // An api endpoint that returns a short list of items
-// app.get('/api/getList', (req,res) => {
-//     let list = ["item1", "item2", "item3"];
-//     res.json(list);
-//     console.log('Sent list of items');
-// });
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // Handles any requests that don't match the ones above
-app.get('*', (req:Request,res:Response) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("*", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-io.on('connection', function(socket:Socket){
-    console.log('a user connected');
+io.on("connection", (socket: Socket) => {
+    console.log("a user connected");
 
-    socket.on('createRoom',(name:string) => {
-        console.log("Creating a room")
-        let roomCode:string = RoomGenerator.Generate();
-        let room :Room = RoomGenerator.Room();
+    socket.on("createRoom", (name: string) => {
+        console.log("Creating a room");
+        const roomCode: string = Generate();
+        const room: Room = EmptyRoom();
         room.code = roomCode;
-        const participant:Participant = {
-            name: name
+        const participant: Participant = {
+            name,
         };
         room.participants.push(participant);
         console.log(roomCode);
         currentRooms.push(room);
         console.log(currentRooms);
         socket.join(roomCode);
-        socket.emit('roomCreated',roomCode);
+        socket.emit("roomCreated", roomCode);
     });
 
-    socket.on('joinRoom',(roomCode:string,participantName:string)=>{
+    socket.on("joinRoom", (roomCode: string, participantName: string) => {
 
-        const roomToEnter = currentRooms.filter(roomObject =>{
-           return roomObject.code === roomCode
+        const roomToEnter = currentRooms.filter((roomObject) => {
+           return roomObject.code === roomCode;
         });
-        
-        const participantToAdd : Participant = {
-            name:participantName
+
+        const participantToAdd: Participant = {
+            name: participantName,
         };
 
-        if(roomToEnter.length === 1){
+        if (roomToEnter.length === 1) {
             roomToEnter[0].participants.push(participantToAdd);
             socket.join(roomCode);
-            socket.emit('joinRoomSuccess',roomCode)
-        }else{
-            socket.emit('joinRoomFailed');
-        } 
+            socket.emit("joinRoomSuccess", roomCode);
+        } else {
+            socket.emit("joinRoomFailed");
+        }
     });
 
-    socket.on('addNote',(roomCode:string,noteToAdd:Note)=>{
-        let roomToEnter = currentRooms.filter(roomObject =>{
-            return roomObject.code === roomCode
+    socket.on("addNote", (roomCode: string, noteToAdd: Note) => {
+        const roomToEnter = currentRooms.filter((roomObject) => {
+            return roomObject.code === roomCode;
          })[0];
 
-         let existingNote = roomToEnter.notes.filter(note =>{
+        const existingNote = roomToEnter.notes.filter((note) => {
             return note.id === noteToAdd.id && note.author === noteToAdd.author;
          })[0];
 
          // If it is an existing note just update the location
-         if(existingNote){
+        if (existingNote) {
             existingNote.positionX = noteToAdd.positionX;
             existingNote.positionY = noteToAdd.positionY;
-         } else{
+         } else {
             roomToEnter.notes.push(noteToAdd);
          }
         console.log(roomToEnter.notes);
-        socket.to(roomCode).emit('noteUpdated',roomToEnter.notes);
-    })
+        socket.to(roomCode).emit("noteUpdated", roomToEnter.notes);
+    });
   });
 const port = process.env.PORT || 5000;
 http.listen(port);
 
-console.log('App is listening on port ' + port);
+console.log("App is listening on port " + port);
